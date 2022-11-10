07b696817bfba2268335852563746d38c2c1c613
LaggySoul <trcalaquian@gmail.com>
2022-11-10 11:47:43 -0800

    Removed key from public

--- scripts/firebaseAPI_TEAM23.js
+++ scripts/firebaseAPI_TEAM23.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: "AIzaSyDRPXK5r8hdQeER1nGJSDsp0T4LhVe1VGU",
-    authDomain: "bby23-23c88.firebaseapp.com",
-    projectId: "bby23-23c88",
-    storageBucket: "bby23-23c88.appspot.com",
-    messagingSenderId: "457171488307",
-    appId: "1:457171488307:web:a327d4a4051da7202bdd46"
+    apiKey:,
+    authDomain:,
+    projectId:,
+    storageBucket:,
+    messagingSenderId:,
+    appId:
 };
 
 //--------------------------------------------
