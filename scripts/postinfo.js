887c73fe21ad8d5ce5b6e025942efa54f7002133
LaggySoul <trcalaquian@gmail.com>
2022-11-16 17:31:48 -0800

    Fixed some bugs with this page

--- scripts/postinfo.js
+++ scripts/postinfo.js
@@ -10,10 +10,14 @@ function getPostInfo(postCode){
                if (size = 1) {
                    var thisPost = posts[0].data();
                    var title = thisPost.title;
+                   var catagory = thisPost.catagory;
                    var author = thisPost.nickname;
+                   var school = thisPost.school;
                    var content = thisPost.content;
                    document.querySelector('.body-title').innerHTML = title;
+                   document.querySelector('.body-catagory').innerHTML = "Catagory: " + catagory;
                    document.querySelector('.body-author').innerHTML = "By: " + author;
+                   document.querySelector('.body-school').innerHTML = "From: " + school;
                    document.querySelector('.body-content').innerHTML = content;
                } else {
                    console.log("Query has more than one data")
